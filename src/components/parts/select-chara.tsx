df6580691bebee9ceeca6700c793eb92f5d0f714
Triple3T <triple@triple-lab.com>
2024-01-23 14:36:29 +0900

    personality bg

--- src/components/parts/select-chara.tsx
+++ src/components/parts/select-chara.tsx
@@ -162,7 +162,7 @@ const SelectChara = ({
         <div className="flex gap-4 p-4 w-full font-onemobile">
           <div className="w-full">
             <div className="text-lg">{t("ui.charaSelect.owned")}</div>
-            <ScrollArea className="mt-2 p-1 w-full h-80 sm:h-96 bg-gray-400/50 rounded">
+            <ScrollArea className="mt-2 p-1 w-full h-80 sm:h-96 bg-gray-400/50 rounded-lg">
               <div className="grid grid-cols-[repeat(auto-fill,_minmax(3rem,_1fr))] sm:grid-cols-[repeat(auto-fill,_minmax(3.5rem,_1fr))] md:grid-cols-[repeat(auto-fill,_minmax(4rem,_1fr))] gap-0.5">
                 {Object.keys(board)
                   .filter(
@@ -170,19 +170,37 @@ const SelectChara = ({
                       !userData?.u.includes(chara) &&
                       (search ? board[chara].n.includes(search) : true)
                   )
-                  .sort((a,b) => board[a].n.localeCompare(board[b].n))
+                  .sort((a, b) => board[a].n.localeCompare(board[b].n))
                   .map((chara) => {
+                    const imgClassNames = ["w-full", "aspect-square"];
+                    switch (board[chara].t[0]) {
+                      case "0":
+                        imgClassNames.push("bg-personality-Cool");
+                        break;
+                      case "1":
+                        imgClassNames.push("bg-personality-Gloomy");
+                        break;
+                      case "2":
+                        imgClassNames.push("bg-personality-Jolly");
+                        break;
+                      case "3":
+                        imgClassNames.push("bg-personality-Mad");
+                        break;
+                      case "4":
+                        imgClassNames.push("bg-personality-Naive");
+                        break;
+                    }
                     return (
-                      <div className="min-w-12 min-h-12 sm:min-w-14 sm:min-h-14 md:min-w-16 md:min-h-16 relative aspect-square">
+                      <div className="min-w-12 min-h-12 sm:min-w-14 sm:min-h-14 md:min-w-16 md:min-h-16 relative aspect-square rounded overflow-hidden">
                         <img
                           key={chara}
                           src={`/charas/${chara}.png`}
-                          className="w-full aspect-square"
+                          className={imgClassNames.join(" ")}
                           onClick={() => {
                             dispatchUserData({ type: "unown", chara: chara });
                           }}
                         />
-                        <div className="absolute w-full left-0 bottom-0 bg-gray-800/75 text-center text-xs py-0.5">
+                        <div className="absolute w-full left-0 bottom-0 bg-slate-100/75 dark:bg-gray-800/75 text-center text-xs py-0.5">
                           {board[chara].n}
                         </div>
                       </div>
@@ -200,25 +218,43 @@ const SelectChara = ({
           </div>
           <div className="w-full">
             <div className="text-lg">{t("ui.charaSelect.unowned")}</div>
-            <ScrollArea className="mt-2 p-1 w-full h-80 sm:h-96 bg-gray-400/50 rounded">
+            <ScrollArea className="mt-2 p-1 w-full h-80 sm:h-96 bg-gray-400/50 rounded-lg">
               <div className="grid grid-cols-[repeat(auto-fill,_minmax(3rem,_1fr))] sm:grid-cols-[repeat(auto-fill,_minmax(3.5rem,_1fr))] md:grid-cols-[repeat(auto-fill,_minmax(4rem,_1fr))] gap-0.5">
                 {userData?.u
                   .filter((chara) =>
                     search ? board[chara].n.includes(search) : true
                   )
-                  .sort((a,b) => board[a].n.localeCompare(board[b].n))
+                  .sort((a, b) => board[a].n.localeCompare(board[b].n))
                   .map((chara) => {
+                    const imgClassNames = ["w-full", "aspect-square"];
+                    switch (board[chara].t[0]) {
+                      case "0":
+                        imgClassNames.push("bg-personality-Cool");
+                        break;
+                      case "1":
+                        imgClassNames.push("bg-personality-Gloomy");
+                        break;
+                      case "2":
+                        imgClassNames.push("bg-personality-Jolly");
+                        break;
+                      case "3":
+                        imgClassNames.push("bg-personality-Mad");
+                        break;
+                      case "4":
+                        imgClassNames.push("bg-personality-Naive");
+                        break;
+                    }
                     return (
-                      <div className="min-w-12 min-h-12 sm:min-w-14 sm:min-h-14 md:min-w-16 md:min-h-16 relative aspect-square">
+                      <div className="min-w-12 min-h-12 sm:min-w-14 sm:min-h-14 md:min-w-16 md:min-h-16 relative aspect-square rounded overflow-hidden">
                         <img
                           key={chara}
                           src={`/charas/${chara}.png`}
-                          className="w-full aspect-square"
+                          className={imgClassNames.join(" ")}
                           onClick={() => {
                             dispatchUserData({ type: "own", chara: chara });
                           }}
                         />
-                        <div className="absolute w-full left-0 bottom-0 bg-gray-800/75 text-center text-xs py-0.5">
+                        <div className="absolute w-full left-0 bottom-0 bg-slate-100/75 dark:bg-gray-800/75 text-center text-xs py-0.5">
                           {board[chara].n}
                         </div>
                       </div>
