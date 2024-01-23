da2693323ca04723042075e7dde9e39eb6752274
Triple3T <triple@triple-lab.com>
2024-01-23 11:22:30 +0900

    Refactor layout and styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -37,44 +37,37 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div
-        className="bg-center bg-cover bg-no-repeat h-screen w-screen absolute top-0 left-0 bottom-0 right-0 opacity-25 dark:brightness-80 dark:contrast-125"
+        className="bg-center bg-cover bg-no-repeat h-screen w-screen fixed top-0 left-0 bottom-0 right-0 opacity-25 dark:brightness-80 dark:contrast-125"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       />
-      <div className="p-24 flex justify-between flex-col items-center overflow-y-auto">
-        <div className="absolute top-0 left-0 p-2 flex gap-2 w-full">
-          <div className="flex-1 flex justify-end">
-            <ModeToggle />
-          </div>
+      <div className="fixed top-0 left-0 flex p-2 gap-2 w-full z-10">
+        <div className="flex-1 flex justify-end">
+          <ModeToggle />
         </div>
-        <h1 className="text-6xl">이름못정함</h1>
-        <h3>누가좀정해주세요감사합니다</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full">
-          <div className="xs:px-10 px-5 py-4">
-            <Link to="/board">
-              <Card className="p-4 text-left">
-                <div className="text-2xl">황크 칠하기</div>
-                <div>나만 황크 안 나와</div>
-              </Card>
-            </Link>
-          </div>
-          <div className="xs:px-10 px-5 py-4">
+      </div>
+      <div className="p-16 relative flex justify-between flex-col items-center w-full h-full">
+        <h1 className="text-3xl">이름못정함</h1>
+        <h3 className="text-sm">누가좀정해주세요감사합니다</h3>
+        <h3 className="text-sm">아말쩝ㅎ;;</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full gap-4 mt-12">
+          <Link to="/board">
             <Card className="p-4 text-left">
-              <div className="text-2xl">보크 칠하기</div>
-              <div>생각보다 많이 필요하더라</div>
+              <div className="text-2xl">황크 칠하기</div>
+              <div>나만 황크 안 나와</div>
             </Card>
-          </div>
-          <div className="xs:px-10 px-5 py-4">
-            <Card className="p-4 text-left">
-              <div className="text-2xl">랭크 올리기</div>
-              <div>랭크작 적어두기 귀찮아</div>
-            </Card>
-          </div>
-          <div className="xs:px-10 px-5 py-4">
-            <Card className="p-4 text-left">
-              <div className="text-2xl">교단 업그레이드</div>
-              <div>루포야 일할 시간이야</div>
-            </Card>
-          </div>
+          </Link>
+          <Card className="p-4 text-left">
+            <div className="text-2xl">보크 칠하기</div>
+            <div>생각보다 많이 필요하대</div>
+          </Card>
+          <Card className="p-4 text-left">
+            <div className="text-2xl">랭크 올리기</div>
+            <div>어디 적어두기 귀찮아</div>
+          </Card>
+          <Card className="p-4 text-left">
+            <div className="text-2xl">교단 업그레이드</div>
+            <div>루포야 일할 시간이야</div>
+          </Card>
         </div>
       </div>
     </ThemeProvider>
