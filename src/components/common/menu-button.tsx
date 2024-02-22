1cc4ae9a1851a1157f08ac19c55445839fd9d19b
Triple3T <triple@triple-lab.com>
2024-02-23 01:17:21 +0900

    add menubutton to easily move menu by menu

---
+++ src/components/common/menu-button.tsx
@@ -0,0 +1,58 @@
+import { useNavigate, matchPath } from "react-router-dom";
+import { t } from "i18next";
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const MenuButton = () => {
+  const navigate = useNavigate();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Menu className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+          <span className="sr-only">Menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuItem
+          onClick={() => navigate("/board")}
+          disabled={matchPath(location.pathname, "/board") ? true : false}
+        >
+          <img
+            src="/icons/Item_Crayon4.png"
+            className="w-4 h-4 inline-block mr-1"
+          />
+          {t("ui.board.title")}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => navigate("/eqrank")}
+          disabled={matchPath(location.pathname, "/eqrank") ? true : false}
+        >
+          <img
+            src="/itemslot/Tab_Equip_Default.png"
+            className="w-4 h-4 inline-block mr-1 rounded-full bg-greenicon"
+          />
+          {t("ui.equiprank.title")}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => navigate("/lab")}
+          disabled={matchPath(location.pathname, "/lab") ? true : false}
+        >
+          <img
+            src="/mainlobby/HousingButton.png"
+            className="w-4 h-4 inline-block mr-1"
+          />
+          {t("ui.lab.title")}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default MenuButton;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "./theme-provider";
-import BackButton from "./common/back-button";
 import { ModeToggle } from "./mode-toggle";
+import BackButton from "./common/back-button";
+import MenuButton from "./common/menu-button";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -8,6 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="fixed top-0 left-0 p-2 w-full flex gap-2 z-20">
         <div className="flex-1 flex gap-2 justify-start">
           <BackButton />
+          <MenuButton />
         </div>
         <div className="flex-1 flex gap-2 justify-end">
           <ModeToggle />
