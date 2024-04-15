8d5b22bd636bb546b4fa1465a0e51c9b19092e80
Triple3T <triple@triple-lab.com>
2024-04-15 20:45:23 +0900

    userdata type changed

--- src/utils/versionMigrate.ts
+++ src/utils/versionMigrate.ts
@@ -7,7 +7,7 @@ const sigConvert = (fdt: string, sig: string) => {
     const board = { b: d.b ?? {}, c: d.c || 0 };
     const pboard = { p: {}, d: [] };
     const nthboard = { n: {} };
-    const eqrank = { r: {}, s: [0, 0, 0] };
+    const eqrank = { r: {}, s: [0, 0, 0], f: [[0, 0, 0]] };
     const unowned = { o: Object.keys(d.b ?? {}), u: d.u ?? [] };
     const lab = { 1: 0, 2: 0 };
     const myhome = { l: 0, r: 0, m: 0, s: 0, a: 0 };
