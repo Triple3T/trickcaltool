54898d92111e27c28c7f79c763c611bce426393f
Triple3T <triple@triple-lab.com>
2024-04-28 10:58:24 +0900

    Add cache control headers to hash api response

--- functions/api/hash.ts
+++ functions/api/hash.ts
@@ -1,5 +1,7 @@
 export async function onRequestGet({ env }) {
     const response = new Response(env.CF_PAGES_COMMIT_SHA);
+    response.headers.append('Cache-Control', 'no-cache, no-store, must-revalidate');
+    response.headers.append('Pragma', 'no-cache');
     return response;
   }
   
