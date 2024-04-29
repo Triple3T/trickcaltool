59cb5e0a06107034591c67183126b1b6af506ad1
Triple3T <triple@triple-lab.com>
2024-04-29 12:48:54 +0900

    test

--- src/setting.tsx
+++ src/setting.tsx
@@ -26,6 +26,7 @@ const SettingCore = () => {
   }, []);
   const installNewVersion = useCallback(async () => {
     setInstallButtonText("ui.index.versionCheck.preparing");
+    // delete must-revalidate cache
     await caches.delete("workbox-mustrevalidate-https://tr.triple-lab.com/");
     navigator.serviceWorker
       .register("/sw.js", { scope: "/", updateViaCache: "none" })
