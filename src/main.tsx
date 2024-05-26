6e347e33ade91e8b6791972ca3a3fa2f1460a2d6
Triple3T <triple@triple-lab.com>
2024-05-27 05:54:21 +0900

    remove periodic update checker

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,6 @@ import "./index.css";
 import "./font.css";
 import "@/locale/localize";
 
-if (typeof window !== "undefined") import("./utils/pwa");
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContext>
