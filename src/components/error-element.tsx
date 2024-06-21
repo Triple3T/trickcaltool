2820ebc577180b45a339577ffd79c0f9493f4d28
Triple3T <triple@triple-lab.com>
2024-06-22 04:11:04 +0900

    fix parsing error

--- src/components/error-element.tsx
+++ src/components/error-element.tsx
@@ -4,28 +4,61 @@ import Error404 from "@/components/errors/404";
 import NeedUpdate from "./errors/need-update";
 import UnknownError from "./errors/unknown-error";
 interface ErrorObjectProps {
-  status: number;
-  error: { message: string; stack: string };
+  name: string;
+  message: string;
+  stack: string;
 }
+interface RouteErrorProps {
+  data: string;
+  error: {
+    columnNumber: number;
+    fileName: string;
+    lineNumber: number;
+    message: string;
+    stack: string;
+  };
+  internal: boolean;
+  status: number;
+  statusText: string;
+}
+type ErrorType = ErrorObjectProps | RouteErrorProps;
 const ErrorElement = () => {
-  const error = useRouteError() as ErrorObjectProps;
+  const error = useRouteError() as ErrorType;
   useEffect(() => {
-    console.log(error);
+    console.log("Error", error);
+    //
   }, [error]);
-  if (error.status === 404) {
-    return <Error404 message={error.error.message} />;
+  // error as RouteErrorProps
+  if ((error as RouteErrorProps).data) {
+    if ((error as RouteErrorProps).status === 404) {
+      return <Error404 message={(error as RouteErrorProps).error.message} />;
+    }
+    return (
+      <UnknownError
+        message={(error as RouteErrorProps).error.message}
+        stack={(error as RouteErrorProps).error.stack}
+      />
+    );
   }
+
+  // error as ErrorObjectProps
   if (
-    error.error.message
+    (error as ErrorObjectProps).message
       .toLowerCase()
-      .includes("failed to fetch dynamically imported module")
+      .includes("dynamically imported module")
   ) {
     return (
-      <NeedUpdate message={error.error.message} stack={error.error.stack} />
+      <NeedUpdate
+        message={(error as ErrorObjectProps).message}
+        stack={(error as ErrorObjectProps).stack}
+      />
     );
   }
   return (
-    <UnknownError message={error.error.message} stack={error.error.stack} />
+    <UnknownError
+      message={(error as ErrorObjectProps).message}
+      stack={(error as ErrorObjectProps).stack}
+    />
   );
 };
 
