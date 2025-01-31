76af27c8fe9b3e4a960ee5bae0e26de6ac466bb2
Triple3T <triple@triple-lab.com>
2025-02-01 03:28:53 +0900

    change description

--- src/locale/localize.ts
+++ src/locale/localize.ts
@@ -5460,7 +5460,7 @@ i18n
               costumeChange: {
                 title: "코스튬 변경",
                 description1: "사도를 선택하고 원하는 코스튬을 입혀주세요",
-                description2: "코스튬은 선택 즉시 로컬 브라우저에만 저장됩니다",
+                description2: "변경한 코스튬은 동기화 시에도 저장됩니다",
               },
               versionCheck: {
                 title: "버전 확인",
