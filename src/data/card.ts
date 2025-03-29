d1d806020a64677aafa1d63996bdaef1dfcbdae9
Triple3T <triple@triple-lab.com>
2025-03-29 21:07:15 +0900

    type refactor, data change

--- src/data/card.ts
+++ src/data/card.ts
@@ -1,29 +1,23 @@
 import card from "./card.json";
+export interface ICardProperty {
+  r: number;
+  p: number | number[];
+  s: number[];
+  c: (number | number[] | "p")[];
+  l: number;
+}
+export type ISpellData = ICardProperty;
+export interface IArtifactData extends ICardProperty {
+  a?: { t: string; l: number[]; c: number[][] };
+}
+export interface CardTypeAllData<T extends ISpellData> {
+  o: number[];
+  l: {
+    [key: string]: T;
+  };
+}
 export default card as {
   r: { s: string; b: string }[];
-  a: {
-    o: number[];
-    l: {
-      [key: string]: {
-        r: number;
-        p: number | number[];
-        s: number[];
-        c: (number | number[] | "p")[];
-        l: number;
-        a?: {t: string; l: number[]; c: number[][]};
-      };
-    };
-  };
-  s: {
-    o: number[];
-    l: {
-      [key: string]: {
-        r: number;
-        p: number | number[];
-        s: number[];
-        c: (number | number[] | "p")[];
-        l: number;
-      };
-    };
-  };
+  a: CardTypeAllData<IArtifactData>;
+  s: CardTypeAllData<ISpellData>;
 };
